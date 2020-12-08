91702a6cf970f72685d5e818c956950277e54820
Kumar44 <744992@cognizant.com>
2020-12-08 20:20:29 +0530

    updated test class

--- src/test/java/com/example/hospitalfrontdesk/HospitalFrontDeskApplicationTests.java
+++ src/test/java/com/example/hospitalfrontdesk/HospitalFrontDeskApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class HospitalFrontDeskApplicationTests {
+public class HospitalFrontDeskApplicationTests {
 	
 	public static Logger logger = LoggerFactory.getLogger(HospitalFrontDeskApplicationTests.class);
 
